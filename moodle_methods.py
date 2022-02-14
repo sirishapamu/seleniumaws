67f1c0427f556ed95574d18bfb225b41786c9d0a
Sirisha M P <sirishampamu@gmail.com>
2022-02-14 12:47:06 -0800

    first commit

--- moodle_methods.py
+++ moodle_methods.py
@@ -5,10 +5,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import moodle_locators as locators
 
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome('C:/Users/Sirisha/PycharmProjects/pythonProject/chromedriver.exe')
+
+driver = webdriver.Chrome(options=options)
 
 
 # Fixture method - to open web browser
